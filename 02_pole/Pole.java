d7b2f54a35cd0750a414ec12b5248f1297fd9bb1
Dušan Pribišan <dusanpribisan@gmail.com>
2015-11-03 17:58:01 +0100

    Oprava drobnej chyby. Jedna dobrá rada. Inak OK

--- 02_pole/Pole.java
+++ 02_pole/Pole.java
@@ -6,7 +6,15 @@
  *
  * purpose:			create an array of user-input size,
  *						fill it with random numbers and print the array out
- *						
+ *				
+ * Až na tú drobnosť čo vidíš je to v podstate dobre spravný program. 
+ * Iné možno trochu lepšie riešenie máš v PoleExtended. Ani to ešte nieje OOP prístup,
+ * ale ak pochopíš PoleExtended a naučíš sa takto programovať si na dobrej ceste 
+ * aby sme prešli na čisto OOP riešenie.
+ * 
+ * Ešte jedna vec: (to je len a len dobrá rada, ak nechceš neriaď sa ňou):
+ * nauč sa písať komentáre do programu, aj v takýchto krátkych ... čo tým myslým vidíš dole
+ *		
  */
 
 import java.util.Scanner;
@@ -18,6 +26,7 @@ public class Pole {
 		Scanner vstup = new Scanner(System.in);
 		Random randomCeleCislo = new Random();
 
+		// inicializácia poľa
 		System.out.print("Zadajte pocet cislic: ");
 		final int pocet = vstup.nextInt();
 
@@ -25,20 +34,24 @@ public class Pole {
 
 		vstup.close(); 
 		
-		for (int i=0; i<pole.length; i++){
+		// naplnenie poľa náhodnými číslami
+		for (int i=0; i<pole.length; i++){ // tu pristupuješ namiesto pocet k pole.length
 			pole[i] = randomCeleCislo.nextInt(100);
 		}
-
+		
+		// vypísanie poľa pred utriedením 
 		System.out.println("Pred zoradenim:");
-		for (int i=0; i<pocet; i++){
+		for (int i=0; i<pole.length; i++){ // prečo TU a ...
 			System.out.print(pole[i] + " ");
 		}
 		System.out.println();
 
+		// utriedenie poľa
 		Arrays.sort(pole);
 
+		// vypísanie poľa po utriedení
 		System.out.println("Po zoradeni:");
-		for (int i=0; i<pocet; i++){
+		for (int i=0; i<pole.length; i++){ // ... a TU už nie?
 			System.out.print(pole[i] + " ");
 		}
 		System.out.println();
