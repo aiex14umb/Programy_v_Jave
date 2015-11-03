56699b619e3d55e60de4990e167347f7068dbba9
Dušan Pribišan <dusanpribisan@gmail.com>
2015-11-03 17:15:18 +0100

    Význam operátoru + v metóde System.out.print()

--- 01_hello_world_extended/HelloWorldExtended.java
+++ 01_hello_world_extended/HelloWorldExtended.java
@@ -13,11 +13,13 @@ public class HelloWorldExtended {
 	public static void main(String[] args){
 		System.out.println("Hello, World!\n");
 
+		// Zrejme si nepochopil úplne presne čo som ti chcel ukázať o operátore "+" v metóde System.out.print().
+		// Tvoj kód volá 2x metódu System.out.print() a v podstate vždy vytlačí medzeru a potom číslo a toto sa opakuje.
+		// Dá sa to napísať do jedného volania funkcie, tak ako ukazujem:
 		for(int n=1; n<=100; n++){
-			System.out.print(" ");
-			System.out.print(n);
+			System.out.print(" " + n);
 		}
-
+		
 		System.out.println();
 	}
 }
