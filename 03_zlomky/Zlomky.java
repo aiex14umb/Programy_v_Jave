c5038132e28778d194400bbee3623cfac7e34782
tukusejssirs <tukusejssirs@github.com>
2015-11-04 19:06:07 +0100

    v5.0.1

--- 03_zlomky/Zlomky.java
+++ 03_zlomky/Zlomky.java
@@ -3,12 +3,11 @@
  *
  * version:			0.5
  *
- *
  * compilation:	javac Zlomky.java
  * execution:		java Zlomky
  *
  * purpose:			addition of two fractions
- * 
+ *
  */
 
 import java.util.Scanner;
@@ -16,23 +15,20 @@ import java.util.regex.*;
 import static java.lang.Math.abs;
 
 public class Zlomky {
-	// Use public to make it simple (not always the best choice)
+	// The use of public datatypes makes it simple, but it is not always the best choice
 	public static Scanner vstup = new Scanner(System.in);  // class variable
 	public static Pattern whitespace=vstup.delimiter();  // set to default (whitespace) delimiter
 	public int citatel;  // instance (i.e. class variable’s) variable
 	public int menovatel;  // instance (i.e. class variable’s) variable
-	
+
+	// Set options
 	public static void main(String[] args){
 		if (args[0] == "-mixed"){
-			public static = 1;
+			public static int option = 1;
 		}
-		/*
-		 * Vytvorenie prvého zlomku 
-		 */
-		// nová inštancie typu Zlomky, toto je základ
-		Zlomky zlomok1 = new Zlomky(); 
-		
-		// načítanie čitateľa a menovateľa
+
+		// First fraction
+		Zlomky zlomok1 = new Zlomky();
 		System.out.print("Zadajte prvy zlomok v tvare a/b: ");
 		vstup.useDelimiter("/");  // set delimiter to `/'
 		zlomok1.citatel = vstup.nextInt();
@@ -41,10 +37,8 @@ public class Zlomky {
 		zlomok1.menovatel = vstup.nextInt();
 		vstup.nextLine();
 
-		// nová inštancie typu Zlomky, toto je základ
-		Zlomky zlomok2 = new Zlomky(); 
-		
-		// načítanie čitateľa a menovateľa
+		// Second fraction
+		Zlomky zlomok2 = new Zlomky();
 		System.out.print("Zadajte druhy zlomok v tvare a/b: ");
 		vstup.useDelimiter("/");  // set delimiter to `/'
 		zlomok2.citatel = vstup.nextInt();
@@ -53,45 +47,46 @@ public class Zlomky {
 		zlomok2.menovatel = vstup.nextInt();
 		vstup.close();
 		System.out.println();
-		
-		/*
-		 * Teraz konečne použijeme našu metódu sčítaj, ktorej výsledok bude nová inštancia.
-		 */
+
 		Zlomky zlom3 = scitaj(zlomok1, zlomok2);
 
+		// Integer in mixed numerals
+		int integer = zlomok.citatel / zlomok.menovatel;
+
 		System.out.print("Zlomok3: " + zlom3.citatel + "/" + zlom3.menovatel);
+
+		if (args[0]=="-mixed"){
+			if (menovatel == 1){  // e.g., 500/5 + 400/100 = 104
+				System.out.println(zlom1.citatel + "/" + zlom1.menovatel + " + " + zlom2.citatel + "/" + zlom2.menovatel + " = " + integer);
+			}else{
+				System.out.println(zlom1.citatel + "/" + zlom1.menovatel + " + " + zlom2.citatel + "/" + zlom2.menovatel + " = " + integer + " " + citatel_new + "/" + menovatel);
+			}
+		}else{
+			System.out.println(zlom1.citatel + "/" + zlom1.menovatel + " + " + zlom2.citatel + "/" + zlom2.menovatel + " = " + citatel + "/" + menovatel);
+		}
 	}
-	
-	/*
-	 * Podľa zadania máš vytvoriť metódu scitaj() ktorá vráti novú inštanciu typu Zlomky
-	 * takže si tú metódu musíme naprogramovať :)
-	 */
-	// vytvorili sme verejnú funkciu s názvom sčítaj, ktorá vracia typ: Zlomky a berie 
-	// ako svoj parameter premennú zlom ktorá je tiež typu Zlomky
-	// P.S.: Typ Zlomky je nami vytvorený dátový typ v podstate je to to isté, ako int, char, boolean a pod.
-	
+
 	/**
 	 * Sčíta dva zlomky
-	 * 
+	 *
 	 * @param zlom - prvý zlomok
 	 * @param zlom2 - druhý zlomok
-	 *  
+	 *
 	 * @return - inštancia obsahujúca súčet zlomkov
 	 */
 	public static Zlomky scitaj(Zlomky zlom, Zlomky zlom2) {
 		Zlomky zlomok = new Zlomky();
-		
-		zlomok.citatel = (zlom.citatel * zlom2.menovatel) 
-				+ (zlom2.citatel * zlom.menovatel);
-		zlomok.menovatel = (zlom.menovatel * zlom2.menovatel);
 
-		Zlomky vysledok = zjednodus(zlomok);  // priradit hodnotu zjednodus();
-		
-		// a konečne o čom je celá úloha vrátimue NOVÚ INŠTANCIU typu Zlomky
+		zlomok.citatel = zlom.citatel * zlom2.menovatel + zlom2.citatel * zlom.menovatel;
+		zlomok.menovatel = zlom.menovatel * zlom2.menovatel;
+
+		Zlomky vysledok = zjednodus(zlomok);
+
 		return vysledok;
 	}
 
-	public static Zlomky zjednodus(Zlomky zlomok){  // Simplify a fraction
+	// Simplify the fraction
+	public static Zlomky zjednodus(Zlomky zlomok){
 		Zlomky zlom_pom = new Zlomky();
 		zlom_pom.citatel = abs(zlomok.citatel);
 		zlom_pom.menovatel = abs(zlomok.menovatel);
@@ -113,9 +108,9 @@ public class Zlomky {
 		}
 	}
 
-	public static Zlomky zlozitopis(Zlomky zlomok){  // Create mixed numerals
+	// Create mixed numerals
+	public static Zlomky zlozitopis(Zlomky zlomok){
 		Zlomky zloz = new Zlomky();
-		int integer = zlomok.citatel / zlomok.menovatel;
 		zloz.citatel = zlomok.citatel % zlomok.menovatel;
 		zloz.menovatel = zlomok.menovatel;
 	}
